d723abf548a3e4cb5f056c47a6bc8166a6d79626
chinesekara <1169668107boo@gmail.com>
2018-09-13 20:18:02 -0400

    first commit

---
+++ src/hello/world/HelloWorld.java
@@ -0,0 +1,27 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package hello.world;
+
+import java.util.Scanner;
+
+/**
+ *
+ * @author Jin
+ */
+public class HelloWorld {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        Scanner sc=new Scanner(System.in);
+        String name; 
+        name=sc.next();
+        System.out.println("Hello World"+name+"!");
+    }
+    
+}
