e5f04bdb52cacc54cda5ff72baf7ef3b904d2ccd
chinesekara <1169668107boo@gmail.com>
2018-09-13 20:51:27 -0400

    input as choice

--- src/hello/world/HelloWorld.java
+++ src/hello/world/HelloWorld.java
@@ -18,10 +18,25 @@ public class HelloWorld {
      */
     public static void main(String[] args) {
         // TODO code application logic here
-        Scanner sc=new Scanner(System.in);
-        String name; 
-        name=sc.next();
-        System.out.println("Hello World"+name+"!");
+        Scanner sc = new Scanner(System.in);
+
+        String name = "";
+        int choice = -1;
+        while (choice < 4) {
+            System.out.print("Please input your choice (1,2,3)");
+            choice = sc.nextInt();
+
+            if (choice == 1) {
+                name = "Benson Hu";
+            } else if (choice == 2) {
+                name = "Zheng Huang";
+            } else if (choice == 3) {
+                name = "Benson Hu, Zheng Huang";
+            }
+            if (choice < 4) {
+                System.out.println("Hello World " + name + "!");
+            }
+        }
+        System.out.println("Program exits! ");
     }
-    
 }
